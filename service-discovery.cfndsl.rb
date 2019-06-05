ed092600c2b90765848321ca52e47535b5bbb1c4
Guslington <guslington@gmail.com>
2019-06-05 13:01:17 +1000

    refactor

--- service-discovery.cfndsl.rb
+++ service-discovery.cfndsl.rb
@@ -1,7 +1,9 @@
 CloudFormation do
 
-  if defined? namespace
+  raise "config `namespace` must be defined" if !defined? namespace
 
+  case type.downcase
+  when 'public'
     ServiceDiscovery_PrivateDnsNamespace(:PrivateDnsNamespace) {
       Description FnSub("Private namespace #{namespace} for the ${EnvironmentName} environment")
       Name FnSub(namespace)
@@ -12,7 +14,17 @@ CloudFormation do
       Value(Ref(:PrivateDnsNamespace))
       Export FnSub("${EnvironmentName}-#{component_name}-PrivateDnsNamespace")
     }
+  when 'private'
+    ServiceDiscovery_PrivateDnsNamespace(:PublicDnsNamespace) {
+      Description FnSub("Public namespace #{namespace} for the ${EnvironmentName} environment")
+      Name FnSub(namespace)
+    }
 
+    Output(:PublicDnsNamespace) {
+      Value(Ref(:PublicDnsNamespace))
+      Export FnSub("${EnvironmentName}-#{component_name}-PublicDnsNamespace")
+    }
+  when 'http'
     ServiceDiscovery_HttpNamespace(:HttpNamespace) {
       Description FnSub("HTTP namespace #{namespace} for the ${EnvironmentName} environment")
       Name FnSub(namespace)
@@ -22,7 +34,8 @@ CloudFormation do
       Value(Ref(:HttpNamespace))
       Export FnSub("${EnvironmentName}-#{component_name}-HttpNamespace")
     }
-
+  else
+    raise "namespace `type` must be one of public | private | http"
   end
 
 end
